2ed6298fb0c8d3304d6cd4bfb36e50e9efe35512
Jean Davilus <jsd6@njit.edu>
2017-05-15 15:59:41 -0400

    starting

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Cilantro.', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beef Jerky.', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Bypass', True);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Calm Tornado.', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Time Stopper.', false);
+
